a5da37e351c9b674bfba69a1cc4b2bcd02a91d33
NestorTangkeko <NestorTangkeko>
2025-07-03 19:01:57 +0800

    Daniel 2025Jul031902: Dashboard UI.

---
+++ src/features/dashboard-and-reports/dashboard/types.tsx
@@ -0,0 +1,26 @@
+export type ticketType = {
+    ticket_id : string;
+    ticket_service : string;
+    ticket_status : number;
+    ticket_level : string;
+    ticket_parent_reference : string;
+    ticket_head_reference : string;
+    ticket_counter : string;
+    ticket_support : string;
+    ticket_create_datetime : Date | null;
+    ticket_queue_datetime : Date | null;
+    ticket_assigned_datetime : Date | null;
+    ticket_now_serving_datetime : Date | null;
+    ticket_served_datetime : Date | null;
+    ticket_no_show_datetime : Date | null;
+    ticket_cancelled_datetime : Date | null;
+    ticket_reason_code : string;
+    ticket_trip_number : string;
+    ticket_trucker_id : string;
+    ticket_trucker_name : string;
+    ticket_vehicle_type : string;
+    ticket_plate_num : string;
+    ticket_remarks1 : string;
+    ticket_remarks2 : string;
+    ticket_remarks3 : string;
+}

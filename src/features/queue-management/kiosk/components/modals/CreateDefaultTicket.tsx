96071aeaf442b4d2a80d516ab87e9d8dbc9f73d0
winnerdj <daniel.manalo@kerrylogistikus.com>
2025-09-04 14:37:25 +0800

    Daniel_2025Sept041324: Update printed ticket size

--- src/features/queue-management/kiosk/components/modals/CreateDefaultTicket.tsx
+++ src/features/queue-management/kiosk/components/modals/CreateDefaultTicket.tsx
@@ -95,7 +95,7 @@ const CreateDefaultTicket: React.FC<{
                         margin: 5px 0;
                     }
                     .ticket-header {
-                        font-size: 20px;
+                        font-size: 35px;
                         padding: 0 0 20px 0;
                     }
                     .ticket-number {
@@ -107,7 +107,7 @@ const CreateDefaultTicket: React.FC<{
                         border-bottom: 13px solid #000;
                     }
                     .ticket-message {
-                        font-size: 40px;
+                        font-size: 50px;
                         font-weight: 500;
                         text-align: center;
                     }
